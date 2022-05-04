61076c4cb73e2bdc29b7637b7592bb1cfc28bb8e
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-04 16:13:39 -0300

    SPA control

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const { dbConnection } = require('./database/config');
+const res = require('express/lib/response');
 
 const PORT = process.env.PORT || 3000;
 
@@ -29,6 +31,11 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
+
+// Lo último
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(PORT, () => {
 	console.log('Servidor corriendo en el puerto ' + process.env.PORT);
