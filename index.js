00c1dd525523a839c3eb95f64b9e1899f34e9fe3
Juan Matias Gomez Andersen <jmgametal@gmail.com>
2022-05-04 01:40:35 -0300

    Test app crashed 01

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.use('/api/login', require('./routes/auth'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log('Servidor corriendo en el puerto ' + process.env.port);
 });
